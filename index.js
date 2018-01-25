3af043574f8ca46e69e692c89c6d331e8f2eddf9
Shawn T <shawn@huthunt.com>
2018-01-25 18:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,36 @@ const logDriversByHometown = function(array, location){
 
   return array;
 };
+
+const driversByRevenue = function(array){
+  const newArray = [...array];
+
+  newArray.sort(function (a, b) {
+    return a.revenue - b.revenue;
+  });
+
+  return newArray;
+
+};
+
+const driversByName = function(array){
+  const newArray = [...array];
+
+  newArray.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+
+  return newArray;
+};
+
+const totalRevenue = function(array){
+  const getRev = function (agg, el, i, arr){
+    return agg + el.revenue;
+  };
+
+  return array.reduce(getRev, 0);
+};
+
+const averageRevenue = function(array){
+  return totalRevenue(array) / array.length;
+};
